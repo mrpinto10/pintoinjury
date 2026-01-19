9856f9609f33066a10329145dc2bf243c3e5237b
Oscar Pinto <oscar@pintoca.com>
2026-01-19 10:36:14 -0800

    Update site title and description to Pinto Injury Law

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Pinto Injury Law';
+export const SITE_DESCRIPTION =
+  'Pre-litigation personal injury guidance for Ontario, Upland, and Rancho Cucamonga, California.';
+
