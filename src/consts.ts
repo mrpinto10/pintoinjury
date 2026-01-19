d86a7654795267f4551cc008aa441ebb698977cf
Oscar Pinto <oscar@pintoca.com>
2026-01-19 15:25:01 -0800

    Update site content and contact page

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Pinto Injury Law';
+export const SITE_TITLE = 'Pinto Injury Resources';
 export const SITE_DESCRIPTION =
-  'Pre-litigation personal injury guidance for Ontario, Upland, and Rancho Cucamonga, California.';
+  'General information about California personal injury claims and guidance on next steps. Not a law firm.';
+
 
